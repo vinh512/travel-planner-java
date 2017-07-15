870b8f7fbbe27b74f1e9078aa6dc3478388611c1
vinh huynh <vvvhuynh@yahoo.com>
2017-07-15 15:46:56 -0500

    implement budget & time method

--- TravelPlanner.java
+++ TravelPlanner.java
@@ -1,18 +1,35 @@
 import java.util.Scanner;
 
 class TravelPlanner {
+
   public static void main(String[] args) {
     greet();
+    budgetTime();
   }
 
   public static void greet() {
     Scanner input = new Scanner(System.in);
-
     System.out.println("Welcome to Travel Planner!");
     System.out.print("What is your name? ");
     String name = input.nextLine();
     System.out.print("Nice to meet you " + name + ". Where are you traveling to? ");
     String destination = input.nextLine();
     System.out.println("Great! " + destination + " Sounds like a fun destination.");
+    System.out.println("\n**********\n");
   }
+
+  public static void budgetTime() {
+    Scanner input = new Scanner(System.in);
+    System.out.print("How many days are you going to stay there? ");
+    int daysStayed = input.nextInt();
+    System.out.print("How much money(USD) are you planning to spend on your trip? ");
+    float budget = input.nextFloat();
+    float hours = daysStayed * 24;
+    float minutes = hours * 60;
+    float budgetPerDay = budget / daysStayed;
+    System.out.println("If you are traveling for " + daysStayed + " days, that is the same as " + hours + " hours or " + minutes + " minutes.");
+    System.out.println("If your budget is $" + budget + ", that is $" + budgetPerDay + " per day.");
+    System.out.println("\n**********\n");
+  }
+
 }
